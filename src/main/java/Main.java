c098d0b4cfb5007e5b13cbd635898fdf16970c1b
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-12-08 10:02:13 +0530

    Add d method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
     }
+    private void d(){
+        for (int i=0; i<10; i++){
+            System.out.println(i);
+        }
+    }
 }
